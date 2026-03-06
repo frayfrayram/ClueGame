f36631a0099d8c17ad7292654ce4a5c4be2b536b
Franklin Rambo <frayfrayram@gmail.com>
2026-03-06 14:22:59 -0700

    addes src and shell implementation

---
+++ src/experiment/TestBoard.java
@@ -0,0 +1,24 @@
+package experiment;
+
+import java.util.Set;
+
+public class TestBoard {
+	
+	public TestBoard() {
+		
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		
+	}
+	
+	public TestBoardCell getCell(int row, int col) {
+		return null;
+		
+	}
+	
+	public Set<TestBoardCell> getTargets(){
+		return null;
+		
+	}
+}
---
+++ src/experiment/TestBoardCell.java
@@ -0,0 +1,34 @@
+package experiment;
+
+import java.util.Set;
+
+public class TestBoardCell {
+	
+	public TestBoardCell(int row, int column) {
+	}
+	
+	public void addAdjacency( TestBoardCell cell ) {
+		
+	}
+	
+	public Set<TestBoardCell> getAdjList(){
+		return null;
+		
+	}
+	
+	public void setRoom(boolean room) {
+		
+	}
+	
+	public boolean isRoom() {
+		return false;
+	}
+	
+	public void setOccupied(boolean occupied) {
+		
+	}
+	
+	boolean getOccupied() {
+		return false;
+	}
+}
