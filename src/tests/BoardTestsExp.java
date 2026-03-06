5a71811c5f77940502873f75704a880d78e9c237
Will Drescher <will_drescher@mines.edu>
2026-03-06 14:41:34 -0700

    franklin is strange

---
+++ src/tests/BoardTestsExp.java
@@ -0,0 +1,47 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import experiment;
+
+class BoardTestsExp {
+
+	@Test
+	void testAdjacency() {
+		TestBoardCell cell = board.getCell(0,0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1,0)));
+		Assert.assertTrue(testList.contains(board.getCell(0,1)));
+		Assert.assertEquals(2,  testList.size());
+	}
+	
+	public void testTargetsNormal() {
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell,3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(6,  targets.size()));
+		Assert.assertTrue(testList.contains(board.getCell(3,0)));
+		Assert.assertTrue(testList.contains(board.getCell(2,1)));
+		Assert.assertTrue(testList.contains(board.getCell(0,1)));
+		Assert.assertTrue(testList.contains(board.getCell(1,2)));
+		Assert.assertTrue(testList.contains(board.getCell(0,3)));
+		Assert.assertTrue(testList.contains(board.getCell(1,0)));
+
+	}
+	
+	public void testTargetsMixed() {
+		board.getCell(0, 2).setOccupied(true);
+		board.getCell(1, 2),setIsRoom(true);
+		TestBoardCell cell = board.getCell(0,3);
+		board.calcTargets(cell,3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(3,  targets.size());
+		Assert.assertTrue(testList.contains(board.getCell(1,2)));
+		Assert.assertTrue(testList.contains(board.getCell(2,2)));
+		Assert.assertTrue(testList.contains(board.getCell(3,3)));
+
+	}
+
+}
