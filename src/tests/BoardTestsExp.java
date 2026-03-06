636b42aa8bc7ef5f3cbf63f2fd47ddb3328f7e7c
Franklin Rambo <frayfrayram@gmail.com>
2026-03-06 15:01:28 -0700

    fixed my DUMB FUCK ROOMATES erros (clear errors btw)

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,6 +1,4 @@
 package tests;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Set;
 
 import org.junit.Assert;
@@ -23,11 +21,12 @@ class BoardTestsExp {
 		Assert.assertEquals(2,  testList.size());
 	}
 	
+	@Test
 	public void testTargetsNormal() {
 		TestBoardCell cell = board.getCell(0, 0);
 		board.calcTargets(cell,3);
 		Set<TestBoardCell> targets = board.getTargets();
-		Assert.assertEquals(6,  targets.size()));
+		Assert.assertEquals(6,  targets.size());
 		Assert.assertTrue(targets.contains(board.getCell(3,0)));
 		Assert.assertTrue(targets.contains(board.getCell(2,1)));
 		Assert.assertTrue(targets.contains(board.getCell(0,1)));
@@ -37,6 +36,7 @@ class BoardTestsExp {
 
 	}
 	
+	@Test
 	public void testTargetsMixed() {
 		board.getCell(0, 2).setOccupied(true);
 		board.getCell(1, 2).setRoom(true);
