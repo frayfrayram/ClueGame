99e02b43e00f2631bb397a7f874a5dc4acaad6a2
Will Drescher <will_drescher@mines.edu>
2026-04-07 13:11:23 -0600

    added GameCrontrolPanel class

---
+++ src/clueGame/GameControlPanel.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GameControlPanel extends JPanel {
+
+	/**
+	 * Constructor for the panel, it does 90% of the work
+	 */
+	public GameControlPanel()  {
+	}
+	
+	
+	/**
+	 * Main to test the panel
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GameControlPanel panel = new GameControlPanel();  // create the panel
+		JFrame frame = new JFrame();  // create the frame 
+		frame.setContentPane(panel); // put the panel in the frame
+		frame.setSize(750, 180);  // size the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
+		frame.setVisible(true); // make it visible
+	}
+}
