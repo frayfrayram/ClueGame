33b87fd82071455d80cd03285b14e7204f4ef276
Will Drescher <will_drescher@mines.edu>
2026-04-09 17:20:49 -0600

    made the base of the CardpanelGUI

--- src/clueGame/CardPanel.java
+++ src/clueGame/CardPanel.java
@@ -25,7 +25,7 @@ public class CardPanel extends JPanel {
 		
 		
 		JPanel cardPanel = new JPanel();
-		cardPanel.setLayout(new GridLayout(3, 0));
+		cardPanel.setLayout(new GridLayout(3, 1));
 		cardPanel.setBorder(new TitledBorder(new EtchedBorder(), "Known Cards"));
 
 		
@@ -34,10 +34,27 @@ public class CardPanel extends JPanel {
 			peoplePanel.setLayout(new GridLayout(2, 0));
 			peoplePanel.setBorder(new TitledBorder(new EtchedBorder(), "People"));
 			
-				JPanel seenPeople = new JPanel();
-				seenPeople.setLayout(new GridLayout(seenPeopleCards, 0));
+				JPanel handPeople = new JPanel();
+				handPeople.setLayout(new GridLayout(2,0));
+				JTextField hPeople = new JTextField("None", 12);
+				JLabel handPeopleLabel = new JLabel("In Hand");
 				
-
+				handPeople.add(handPeopleLabel);
+				handPeople.add(hPeople);
+			
+			peoplePanel.add(handPeople);
+				
+				
+			
+				JPanel seenPeople = new JPanel();
+				seenPeople.setLayout(new GridLayout(2,0));
+				JTextField sPeople = new JTextField("None", 12);
+				JLabel seenPeopleLabel = new JLabel("Seen");
+				
+				seenPeople.add(seenPeopleLabel);
+				seenPeople.add(sPeople);
+				
+			peoplePanel.add(seenPeople);
 			cardPanel.add(peoplePanel);
 			
 			
@@ -46,10 +63,28 @@ public class CardPanel extends JPanel {
 			roomPanel.setLayout(new GridLayout(2, 0));
 			roomPanel.setBorder(new TitledBorder(new EtchedBorder(), "Rooms"));
 			
-				JPanel seenRoom = new JPanel();
-				seenRoom.setLayout(new GridLayout(seenRoomCards, 0));
+			JPanel handRooms = new JPanel();
+			handRooms.setLayout(new GridLayout(2,0));
+			JTextField hRooms = new JTextField("None", 12);
+			JLabel handRoomsLabel = new JLabel("In Hand");
 			
-			cardPanel.add(roomPanel);
+			handRooms.add(handRoomsLabel);
+			handRooms.add(hRooms);
+		
+		roomPanel.add(handRooms);
+			
+			
+		
+			JPanel seenRooms = new JPanel();
+			seenRooms.setLayout(new GridLayout(2,0));
+			JTextField sRooms = new JTextField("None", 12);
+			JLabel seenRoomsLabel = new JLabel("Seen");
+			
+			seenRooms.add(seenRoomsLabel);
+			seenRooms.add(sRooms);
+			
+		roomPanel.add(seenRooms);
+		cardPanel.add(roomPanel);
 			
 			
 			
@@ -58,6 +93,30 @@ public class CardPanel extends JPanel {
 			JPanel weaponPanel = new JPanel();
 			weaponPanel.setLayout(new GridLayout(2, 0));
 			weaponPanel.setBorder(new TitledBorder(new EtchedBorder(), "Weapons"));
+			
+			JPanel handWeapon = new JPanel();
+			handWeapon.setLayout(new GridLayout(2,0));
+			JTextField hWeapon = new JTextField("None", 12);
+			JLabel handWeaponLabel = new JLabel("In Hand");
+			
+			handWeapon.add(handWeaponLabel);
+			handWeapon.add(hWeapon);
+		
+		weaponPanel.add(handWeapon);
+			
+			
+		
+			JPanel seenWeapon = new JPanel();
+			seenWeapon.setLayout(new GridLayout(2,0));
+			JTextField sWeapon = new JTextField("None", 12);
+			JLabel seenWeaponLabel = new JLabel("Seen");
+			
+			seenWeapon.add(seenWeaponLabel);
+			seenWeapon.add(sWeapon);
+			
+		weaponPanel.add(seenWeapon);
+		cardPanel.add(weaponPanel);
+			
 			
 		
 
@@ -80,7 +139,7 @@ public class CardPanel extends JPanel {
 		CardPanel panel = new CardPanel();  // create the panel
 		JFrame frame = new JFrame();  // create the frame 
 		frame.setContentPane(panel); // put the panel in the frame
-		frame.setSize(130, 550);  // size the frame
+		frame.setSize(400, 550);  // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setVisible(true); // make it visible
 	}
