0f7411dc613ced697e2a08388b119e2a87a93055
Will Drescher <will_drescher@mines.edu>
2026-04-23 12:35:01 -0600

    board is drawn and panels appear, need to link to real time gameplay

--- src/clueGame/BoardPanel.java
+++ src/clueGame/BoardPanel.java
@@ -7,7 +7,6 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel {
-	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void paintComponent(Graphics g) {
